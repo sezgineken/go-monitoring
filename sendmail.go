6dab9287d69ec10a4c34573b04e40d0f3f2badbe
sezgineken <111337323+sezgineken@users.noreply.github.com>
2025-05-14 14:06:32 +0300

    İlk yükleme: Go monitoring sistemi eklendi

--- sendmail.go
+++ sendmail.go
@@ -1,35 +1,3 @@
-// FOR LİNUX
-
-/*
-package main
-
-import (
-
-	"fmt"
-	"os/exec"
-	"strings"
-
-)
-
-	func sendMail(subject, body string) {
-		cmd := exec.Command(
-			"msmtp",
-			"--from=default",
-			"-t",
-		)
-
-		message := fmt.Sprintf("To: ensmartit@gmail.com\nSubject: %s\n\n%s", subject, body)
-		cmd.Stdin = strings.NewReader(message)
-
-		err := cmd.Run()
-		if err != nil {
-			fmt.Println("❌ Mail gönderilemedi:", err)
-			return
-		}
-
-		fmt.Println("📧 Uyarı e-postası gönderildi.")
-	}
-*/
 package main
 
 import (
@@ -38,9 +6,9 @@ import (
 )
 
 func sendMail(subject, body string) {
-	from := "ensmartit@gmail.com"
-	password := "amogitqmfaspisuy"
-	to := "ensmartit@gmail.com"
+	from := "youremail@example.com"
+	password := "your-app-password"
+	to := "youremail@example.com"
 
 	msg := "From: " + from + "\n" +
 		"To: " + to + "\n" +
